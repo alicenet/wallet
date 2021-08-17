24012ed76994fa90a062f896b391c55bc29f9308
Cr0wn-Gh0ul <troy.salem.work@gmail.com>
2021-08-17 11:24:36 -0400

    changes for signed MacOS builds

---
+++ notarize.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const { notarize } = require('electron-notarize');
+
+exports.default = async function notarizing(context) {
+  const { electronPlatformName, appOutDir } = context;  
+  if (electronPlatformName !== 'darwin') {
+    return;
+  }
+
+  const appName = context.packager.appInfo.productFilename;
+  const email = process.env.APPLE_EMAIL;
+  const password = process.env.APPLE_PASSWORD;
+  
+  return await notarize({
+    appBundleId: 'com.MadHive.MadWallet',
+    appPath: `${appOutDir}/${appName}.app`,
+    appleId: email,
+    appleIdPassword: password,
+  });
+};
