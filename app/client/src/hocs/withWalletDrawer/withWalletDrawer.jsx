6f8f060dd84025181dfc1dd795cf488135d90218
ACatThatPrograms <25625537+ACatThatPrograms@users.noreply.github.com>
2021-09-02 13:13:09 -0400

    Additional state, and UI interfacing for wallet testing

--- app/client/src/hocs/withWalletDrawer/withWalletDrawer.jsx
+++ app/client/src/hocs/withWalletDrawer/withWalletDrawer.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { Sidebar, Header, Button, Grid, Icon, Image } from 'semantic-ui-react';
+import { Sidebar, Header, Button, Grid, Icon, Image, ButtonGroup } from 'semantic-ui-react';
 import { connect } from 'react-redux';
 import { useBoolToggler } from 'hooks/_hooks.js';
 import MadIcon from "../../Assets/icon.png";
 import PropTypes from 'prop-types';
+import { VAULT_ACTIONS } from 'redux/actions/_actions';
 
 /**
  * Provide wallet tab as a drawer to the passed Component
@@ -12,19 +13,39 @@ import PropTypes from 'prop-types';
  * @returns { Function } - Return same component with toggleWalletDrawer as a prop for opening/closing the wallet drawer
  */
 export default function withWalletDrawer(Component, isOpen = false) {
-    function ComponentWithDrawer({ wallets, ...props }) {
+    function ComponentWithDrawer({ wallets, dispatch, ...props }) {
 
         const [isVisible, toggleSetIsVisible] = useBoolToggler(isOpen);
+        const closeDrawer = () => toggleSetIsVisible(false);
+
+        const [activeWallet, setActiveWallet] = React.useState(false);
+
+        const clickWalletButton = (wallet) => {
+            setActiveWallet(wallet.initId);
+            console.log("Set active wallet to, " + wallet.initId);
+        }
 
         const addWallet = () => {
-            console.log("TBD: Add a wallet via redux actions!")
+            // CAT_TODO: Update to history.push after vault debugging
+            dispatch(VAULT_ACTIONS.addInternalWalletToState())
         }
 
         const WalletButtons = () => {
-            const AddWalletButton = <Button key="add_btn" fluid size="mini" color="green" basic content="Add Wallet" icon={<Icon name="plus" className="bg-transparent" />} labelPosition="right" onClick={addWallet} />
-            const CloseTabButton = <Button key="close_btn" fluid size="mini" color="red" basic content={"Close"} onClick={() => toggleSetIsVisible()} className="mt-4" />
-            const WalletButtons = wallets.internal.map(wallet => <Button key={wallet.id} content={wallet.name} onClick={() => console.log('hit')} />);
-            return [WalletButtons, AddWalletButton, CloseTabButton];
+            const genWalletButtonProps = (wallet) => ({
+                onClick: () => clickWalletButton(wallet), content: wallet.name, key: wallet.initId, disabled: activeWallet === wallet.initId,
+                basic: true, className: "mt-2", color: "grey", fluid: true, icon: "id card outline", size: "small"
+            });
+            const WalletButtons = wallets.internal.map(wallet => <Button {...genWalletButtonProps(wallet)} />);
+
+            const walletActionProps = { basic: true, size: "mini" }
+            const WalletActions = <ButtonGroup size="mini" className="flex justify-between align-between mt-8"
+                buttons={[
+                    { onClick: closeDrawer, className: "w-0.5", color: "grey", icon: "angle double left", key: "close_btn", ...walletActionProps },
+                    { onClick: addWallet, color: "green", content: "Add Wallet", icon: <Icon name="plus" className="bg-transparent" />, key: "add_btn", labelPosition: "right", ...walletActionProps }
+                ]}
+            />
+
+            return [WalletButtons, WalletActions];
         }
 
         return (
---
+++ app/client/src/util/wallet.js
@@ -0,0 +1,12 @@
+import { v4 as uuidv4 } from 'uuid';
+
+// CAT_TODO: Subject to change, not JSdoccing yet
+export function generateStateWalletObject(walletName, privKey, pubKey, pubAdd) {
+    return {
+        name: "A New Wallet",
+        pubAdd: "PUBADD_TEST_STATE_STRING", 
+        pubKey: "PUBK_TEST_STATE_STRING",
+        privkey: "PRIVK_TEST_STATE_STRING",
+        initId: uuidv4(), // Initialized State ID for quick client side identification
+    }
+}
