22e8d98c9478063986e220f894f250601102e1e0
Adrian Bona <adrianbona@outlook.com>
2021-09-16 18:43:05 -0300

    Avoid uppercases, numbers and symbols

--- app/client/src/pages/UseRecoveryPhrase.jsx
+++ app/client/src/pages/UseRecoveryPhrase.jsx
@@ -6,6 +6,8 @@ import { useHistory } from 'react-router-dom';
 
 import trim from 'lodash/trim';
 import uniq from 'lodash/uniq';
+import toLower from 'lodash/toLower';
+import words from 'lodash/words';
 
 import Page from '../layout/Page';
 
@@ -32,8 +34,12 @@ function UseRecoveryPhrase() {
     }, [parsedSeedPhrase]);
 
     React.useEffect(() => {
-        setParsedSeedPhrase(seedPhrase.split(' ').map(word => trim(word)).filter(word => word.length > 0));
+        setParsedSeedPhrase(words(seedPhrase).map(word => trim(word)).filter(word => word.length > 0));
     }, [seedPhrase]);
+
+    const handlePhraseChange = phrase => {
+        setSeedPhrase(toLower(phrase).match(/[a-z ]/g).join(''));
+    }
 
     return (
         <Page>
@@ -81,7 +87,7 @@ function UseRecoveryPhrase() {
 
                     <Container className="flex flex-auto flex-col justify-center gap-10">
 
-                        <TextArea rows={4} value={seedPhrase} onChange={e => setSeedPhrase(e.target.value)}
+                        <TextArea rows={4} value={seedPhrase} onChange={e => handlePhraseChange(e.target.value)}
                                   className="bg-white p-4 rounded border-3 border-gray-400 hover:border-gray-500 focus:border-gray-500 focus:outline-none"/>
 
                         <Container className="flex flex-auto flex-row justify-between">
