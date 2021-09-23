5bb43875f331332ab8791bf4ff07b58f8ce7d090
ACatThatPrograms <25625537+ACatThatPrograms@users.noreply.github.com>
2021-09-23 12:07:52 -0400

    cleanup pr6

--- app/client/src/stories/optOut/VaultOptOutDisclaimer.jsx
+++ app/client/src/stories/optOut/VaultOptOutDisclaimer.jsx
@@ -19,7 +19,7 @@ function VaultOptOutDisclaimer() {
 
                 <Grid.Column width={16} className="p-0 self-center">
 
-                    <Header content="Vault Opt Out" as="h3" className="m-0" />
+                    <Header content="Vault Opt Out" as="h3" className="m-0"/>
 
                 </Grid.Column>
 
@@ -54,7 +54,7 @@ function VaultOptOutDisclaimer() {
 
                         <div>
 
-                            <Button color="orange" basic content="Go Back" className="m-0 h-11" onClick={() => history.goBack()} />
+                            <Button color="orange" basic content="Go Back" className="m-0 h-11" onClick={() => history.goBack()}/>
 
                         </div>
 
@@ -62,11 +62,11 @@ function VaultOptOutDisclaimer() {
 
                             <Button.Group size='large'>
 
-                                <Button color="purple" basic disabled={!isChecked} content="New" className="m-0 h-11" onClick={() => history.push('/YourSeedPhrase')} />
+                                <Button color="purple" basic disabled={!isChecked} content="New" className="m-0 h-11" onClick={() => history.push('/YourSeedPhrase')}/>
 
-                                <Button.Or className="w-0 self-center text-sm" />
+                                <Button.Or className="w-0 self-center text-sm"/>
 
-                                <Button color="purple" basic disabled={!isChecked} content="Existing" className="m-0 h-11" onClick={() => history.push('/YourSeedPhrase')} />
+                                <Button color="purple" basic disabled={!isChecked} content="Existing" className="m-0 h-11" onClick={() => history.push('/YourSeedPhrase')}/>
 
                             </Button.Group>
 
