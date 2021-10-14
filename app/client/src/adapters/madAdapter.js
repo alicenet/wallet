b77acf8c992e9aa6083188a5d6fce820215d0510
ACatThatPrograms <25625537+ACatThatPrograms@users.noreply.github.com>
2021-10-14 17:13:50 -0400

    Add prevTx functionality to adapter

--- app/client/src/adapters/madAdapter.js
+++ app/client/src/adapters/madAdapter.js
@@ -126,6 +126,64 @@ class MadNetAdapter {
         }
     }
 
+    /**
+     * Fetch previous transactions for a given account
+     * @param { Array<String> } addresses - Array of addresses to get previous transactions for
+     * @returns { Array[ [Array<Object>], currentBlock<Int> ] } - Array of previous transactions
+     */
+    async getPrevTransactions(addresses) {
+        try {
+            let blockRange = 256;
+            let currentBlock = await madWallet.Rpc.getBlockNumber()
+            let pTx = [];
+            for (let i = currentBlock; i >= (currentBlock - blockRange); i--) {
+                let block = await madWallet.Rpc.getBlockHeader(i);
+                if (!block["TxHshLst"] || block["TxHshLst"].length <= 0) {
+                    continue;
+                }
+                transactionLoop:
+                for (let l = 0; l < block["TxHshLst"].length; l++) {
+                    let tx = await madWallet.Rpc.getMinedTransaction(block["TxHshLst"][l]);
+                    for (let j = 0; j < tx["Tx"]["Vout"].length; j++) {
+                        for (let k = 0; k < addresses.length; k++) {
+                            let address = addresses[k]["address"].toLowerCase();
+                            let curve = addresses[k]["curve"]
+                            if (curve == 2) {
+                                curve = "02"
+                            }
+                            else {
+                                curve = "01";
+                            }
+                            if ((
+                                tx["Tx"]["Vout"][j]["AtomicSwap"] &&
+                                address == tx["Tx"]["Vout"][j]["AtomicSwap"]["ASPreImage"]["Owner"].slice(4) &&
+                                curve == tx["Tx"]["Vout"][j]["AtomicSwap"]["ASPreImage"]["Owner"].slice(2, 4)
+                            ) ||
+                                (
+                                    tx["Tx"]["Vout"][j]["ValueStore"] &&
+                                    address == tx["Tx"]["Vout"][j]["ValueStore"]["VSPreImage"]["Owner"].slice(4) &&
+                                    curve == tx["Tx"]["Vout"][j]["ValueStore"]["VSPreImage"]["Owner"].slice(2, 4)
+                                ) ||
+                                (
+                                    tx["Tx"]["Vout"][j]["DataStore"] &&
+                                    address == tx["Tx"]["Vout"][j]["DataStore"]["DSLinker"]["DSPreImage"]["Owner"].slice(4) &&
+                                    curve == tx["Tx"]["Vout"][j]["DataStore"]["DSLinker"]["DSPreImage"]["Owner"].slice(2, 4)
+                                )
+                            ) {
+                                pTx = pTx.concat(tx)
+                                continue transactionLoop;
+                            }
+                        }
+                    }
+                }
+            }
+            return [pTx, currentBlock];
+        }
+        catch (ex) {
+            return ([{ error: ex }])
+        }
+    }
+
     /** 
      * Return a getter/setter for a specific redux state value keyChain -- Object depth of max 3 supported
      * @prop {Array} keyChain - An array of nested keys to access the desired value
