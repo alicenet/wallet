60cce40c07a97f0b69bc5c090da9a4e03d5e0b35
ACatThatPrograms <25625537+ACatThatPrograms@users.noreply.github.com>
2021-10-20 15:08:25 -0400

    repatch..

--- app/client/src/stories/addWallet/_addWallet.jsx
+++ app/client/src/stories/addWallet/_addWallet.jsx
@@ -1,14 +1,7 @@
 import addWalletMenu from './AddWalletMenu';
 import generateWallet from './GenerateWallet';
-<<<<<<< HEAD
 import importKeystore from './ImportKeystore';
 
 export const AddWalletMenu = addWalletMenu;
 export const GenerateWallet = generateWallet;
 export const ImportKeystore = importKeystore;
-=======
-
-export const AddWalletMenu = addWalletMenu;
-export const GenerateWallet = generateWallet;
->>>>>>> Add Wallets Menu && Add Internal Wallet
-
