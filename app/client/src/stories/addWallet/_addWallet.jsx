66d3546dcb292c90e35375337b18a7f02db746db
ACatThatPrograms <25625537+ACatThatPrograms@users.noreply.github.com>
2021-10-20 15:08:25 -0400

    repatch..

--- app/client/src/stories/addWallet/_addWallet.jsx
+++ app/client/src/stories/addWallet/_addWallet.jsx
@@ -1,6 +1,8 @@
 import addWalletMenu from './AddWalletMenu';
 import generateWallet from './GenerateWallet';
+import importKeystore from './ImportKeystore';
 
 export const AddWalletMenu = addWalletMenu;
 export const GenerateWallet = generateWallet;
+export const ImportKeystore = importKeystore;
 
