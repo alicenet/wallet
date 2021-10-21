ad65ad1de9fa886b189ef6abba622c180f87a49d
Adrian Bona <adrianbona@outlook.com>
2021-10-21 15:46:55 -0300

    Replace content with placeholder

--- app/client/src/stories/hubs/tabPanes/Datastores.jsx
+++ app/client/src/stories/hubs/tabPanes/Datastores.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
+import { Header, Segment } from 'semantic-ui-react';
 
 export default function Datastores({ wallet }) {
-    return null;
+    return (
+        <Segment placeholder className="m-4">
+            <Header icon>
+                No datastores were found.
+            </Header>
+        </Segment>
+    );
 }
--- app/client/src/stories/hubs/tabPanes/RecentTxs.jsx
+++ app/client/src/stories/hubs/tabPanes/RecentTxs.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
 
+import { Header, Segment } from 'semantic-ui-react';
+
 export default function RecentTxs({ wallet }) {
-    return null;
+    return (
+        <Segment placeholder className="m-4">
+            <Header icon>
+                No recent transactions were found.
+            </Header>
+        </Segment>
+    );
 }
