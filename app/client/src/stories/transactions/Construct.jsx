a3735c26f23f794c34e412da6a2ac164ec66da8e
Adrian Bona <adrianbona@outlook.com>
2021-10-21 18:21:18 -0300

    Testing buttons with icons

--- app/client/src/stories/transactions/Construct.jsx
+++ app/client/src/stories/transactions/Construct.jsx
@@ -1,15 +1,67 @@
 import React from 'react';
 
-import { Grid } from 'semantic-ui-react';
+import { Button, Container, Grid, Header, Icon } from 'semantic-ui-react';
 
-import Page from '../../layout/Page';
+import Page from 'layout/Page';
 
 function Construct() {
 
     return (
         <Page showMenu>
 
-            <Grid textAlign="center" className="m-0"/>
+            <Grid textAlign="center" className="m-0">
+
+                <Grid.Column width={16} className="p-0 self-center">
+
+                    <Header content="Construct a transaction" as="h3" className="m-0"/>
+
+                </Grid.Column>
+
+                <Grid.Column width={8} className="p-0 self-center text-sm">
+
+                    <p>A selector is gonna be here.</p>
+
+                </Grid.Column>
+
+                <Grid.Column width={8} className="p-0 self-center text-sm">
+
+                    <Container className="flex justify-evenly">
+
+                        <Button className="p-1 m-0 relative">
+
+                            <Icon size="tiny" name='plus circle' className="text-sm	absolute p-1 top-0 right-0"/>
+
+                            <Header size="tiny" icon className="uppercase m-0 mx-3">
+
+                                Add<Icon name='chart bar'/>Data Store
+
+                            </Header>
+
+                        </Button>
+
+                        <Button className="p-1 m-0 relative">
+
+                            <Icon size="tiny" name='plus circle' className="text-sm absolute p-1 top-0 right-0"/>
+
+                            <Header size="tiny" icon className="uppercase m-0 mx-3">
+
+                                Add<Icon name='currency'/>Value Store
+
+                            </Header>
+
+                        </Button>
+
+                    </Container>
+
+                </Grid.Column>
+
+                <Grid.Column width={16} className="p-0 self-center">
+
+                    <Button color="teal" content='Send Transaction' disabled className="m-0"/>
+
+                </Grid.Column>
+
+            </Grid>
 
         </Page>
     )
