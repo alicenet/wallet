dc5759e295a031cc670c37cb82cf6df0e2af2da5
Adrian Bona <adrianbona@outlook.com>
2021-10-29 15:53:27 -0300

    Adding loading elements

--- app/client/src/stories/transactions/loading/LoadingModule.jsx
+++ app/client/src/stories/transactions/loading/LoadingModule.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Button, Grid } from 'semantic-ui-react';
+import { Button, Grid, Header, Loader } from 'semantic-ui-react';
 import { useDispatch } from 'react-redux';
+
 import { TRANSACTION_ACTIONS } from 'redux/actions/_actions';
 import Page from 'layout/Page';
 
@@ -15,11 +16,36 @@ function LoadingModule() {
     return (
         <Page showMenu>
 
-            <Grid textAlign="center" className="m-0">
+            <Grid textAlign="center" className="m-0" container>
 
                 <Grid.Column width={16} className="p-0 self-center">
 
-                    <Button color="teal" loading onClick={handleOnClick} className="m-0"/>
+                    <Header content="Awaiting Receipt..." as="h4" className="m-0"/>
+
+                </Grid.Column>
+
+                <Grid.Column width={16} className="p-0 self-center">
+
+                    <Loader active inline='centered' size="massive"/>
+
+                </Grid.Column>
+
+                <Grid.Column width={16} className="p-0 self-center">
+
+                    <p className="text-sm text-yellow-600">
+                        <strong>
+                            If you don&apos;t feel like waiting, you don&apos;t have to!
+                            <br/>
+                            Notifications will appear regarding transaction updates.
+                        </strong>
+                    </p>
+
+                </Grid.Column>
+
+
+                <Grid.Column width={16} className="p-0 self-center">
+
+                    <Button color="teal" loading onClick={handleOnClick} className="m-0 w-52 h-8"/>
 
                 </Grid.Column>
 
@@ -27,7 +53,6 @@ function LoadingModule() {
 
         </Page>
     )
-
 }
 
 export default LoadingModule;
