088a062f5cbe06ba850e0140d344a5d4bb7fbf72
Adrian Bona <adrianbona@outlook.com>
2021-11-09 11:41:14 -0300

    Simplified padding

--- app/client/src/stories/hubs/ReturningUserHub.jsx
+++ app/client/src/stories/hubs/ReturningUserHub.jsx
@@ -125,7 +125,7 @@ export default function Hub() {
 
                     </Grid.Column>
 
-                    <Grid.Column width={openDrawer ? 12 : 14} className="flex flex-col duration-300 transition-transform transition-width p-0 pr-4 pt-4 pb-4">
+                    <Grid.Column width={openDrawer ? 12 : 14} className="flex flex-col duration-300 transition-transform transition-width p-4 pl-0">
 
                         <Container className="flex flex-col">
                             {selectedWallet ?
