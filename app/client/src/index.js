31c68fc143d3d85f9c7336059fd37f2936ed703f
ACatThatPrograms <25625537+ACatThatPrograms@users.noreply.github.com>
2021-11-10 16:50:52 -0500

    A little more

--- app/client/src/index.js
+++ app/client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import RApp from './R_App.jsx';
+import App from 'App.jsx';
 import * as serviceWorker from './serviceWorker';
 import 'react-toastify/dist/ReactToastify.css';
 import 'semantic-ui-less/semantic.less'
@@ -8,12 +8,12 @@ import 'style/tailwind.css';
 import 'style/main.scss';
 
 /* Redux Store */
-import store from "redux/store/store.js"; // Redux store if using
+import store from "redux/store/store.js";
 import { Provider } from "react-redux";
 
 ReactDOM.render(
   <Provider store={store}>
-    <RApp/>
+    <App/>
   </Provider>,
   document.getElementById('root')
 );
