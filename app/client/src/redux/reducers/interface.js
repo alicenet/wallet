29e78e672f8f8e581844a4ba09f8af8909d23682
Adrian Bona <adrianbona@outlook.com>
2021-12-02 15:00:01 -0300

    First draft on adding reducers

--- app/client/src/redux/reducers/interface.js
+++ app/client/src/redux/reducers/interface.js
@@ -4,6 +4,7 @@ import { INTERFACE_ACTION_TYPES } from '../constants/_constants';
 const initialInterfaceState = {
     globalLoading: false, // Is there a global loading state?
     showDebug: true, // Is the state debug overlay showing?
+    useDarkTheme: false, // A global setting to choose the dark theme
 }
 
 /* Modal Reducer */
@@ -17,11 +18,18 @@ export default function interfaceReducer(state = initialInterfaceState, action) 
             });
 
         case INTERFACE_ACTION_TYPES.DEBUG_TOGGLE_SHOW_DEBUG:
-            return Object.assign({}, state,  {
+            return Object.assign({}, state, {
                 showDebug: typeof action.payload !== 'undefined' ? action.payload : !state.showDebug,
             });
 
-        default: return state;
+        case INTERFACE_ACTION_TYPES.TOGGLE_THEME:
+            state.useDarkTheme ? window.setLight() : window.setDark();
+            return Object.assign({}, state, {
+                useDarkTheme: !state.useDarkTheme,
+            });
+
+        default:
+            return state;
 
     }
 
