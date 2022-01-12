5761ac47ae8dcb6dc29a13d8e3bf371fd9a2f679
ACatThatPrograms <25625537+ACatThatPrograms@users.noreply.github.com>
2022-01-12 09:44:53 -0500

    Update ImportPrivateKeyForm.jsx

--- app/client/src/components/keystore/ImportPrivateKeyForm.jsx
+++ app/client/src/components/keystore/ImportPrivateKeyForm.jsx
@@ -30,7 +30,9 @@ export default function ImportPrivateKeyForm({ submitText, submitFunction, cance
     const submit = () => {
         onSubmit( async () => {
             setLoading(true);
-            await utils.generic.waitFor(0); // No idea why this works and lets the component update  -- Feel free to investigate in downtime.
+            await utils.generic.waitFor(0); // Frees thread long enough for loader ui render to propagate. 
+            // The next function eventually calls a low nested syncronously blocking loop that prevents the render, this await will alow it to catch.
+            // It is known this is wonky, and we all hate it, but many calls in the depedency chain have yet to be promisified
             verifyPrivKey();
         });
     }
