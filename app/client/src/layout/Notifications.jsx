467ed316d18319615fdcf3c88a39d85af6b08d6e
RicardoFrabasile <rfrabasile@gmail.com>
2022-01-28 16:28:00 -0300

    Initial state integration

--- app/client/src/layout/Notifications.jsx
+++ app/client/src/layout/Notifications.jsx
@@ -1,10 +1,42 @@
 import React, { useState } from 'react';
 import { Container, Label, Icon, Menu, Popup, Message } from 'semantic-ui-react';
+import { MODAL_ACTION_TYPES } from 'redux/constants/_constants';
+import { electronStoreCommonActions } from 'store/electronStoreHelper';
+import { useDispatch, useSelector } from 'react-redux';
+import { toast } from 'react-toastify';
+import { SyncToastMessageSuccess } from 'components/customToasts/CustomToasts';
+import { VAULT_ACTIONS } from 'redux/actions/_actions';
 
-function Notification({ notification }){
-    return <Message className="cursor-pointer">
-            <div className="font-bold text-sm">{notification.title}</div>
-            <p className="text-xs">{notification.message}</p>
+function Notification({ notification, onClose }){
+    const dispatch = useDispatch();
+    const { wallets } = useSelector(state => (
+        { wallets: state.vault.wallets }
+    ));
+    return <Message warning className="cursor-pointer" onClick={
+        () => {
+            dispatch({
+                type: MODAL_ACTION_TYPES.OPEN_PW_REQUEST, payload: {
+                    reason: "Vault Synchronization | " + notification,
+                    cb: async (password) => {
+                        await electronStoreCommonActions.updateVaultWallets(password, wallets)
+                        toast.success(<SyncToastMessageSuccess title="Success" message={notification} />, {
+                            position: "bottom-right",
+                            autoClose: 2400,
+                            delay: 500,
+                            closeOnClick: true,
+                            pauseOnHover: true,
+                            draggable: true,
+                        });
+                        //clear wallets
+                        //dispatch(VAULT_ACTIONS.syncUnsavedWallets())
+                    }
+                }
+            })
+            toast.dismiss()
+            onClose()
+        }
+    }>
+            <div className="font-bold text-sm">Vault Update Request</div>
         </Message>
 }
 
@@ -13,7 +45,8 @@ export function Notifications({ notifications = [] }){
     return <Popup size="mini"
                 content={<Container className="w-64">
                             <div className="text-right cursor-pointer" onClick={() => setIsOpen(false)}><Icon name="close" className="mx-0"/></div>
-                            <div>{notifications.length <= 0 ? "No pending alerts" : notifications.map(n => <Notification notification={n} key={n.id}/>)}</div>
+                            {/*We are only showing one of the notifications */}
+                            <div>{notifications.length <= 0 ? "No pending alerts" : <Notification notification={notifications[0]} onClose={() => setIsOpen(false)}/>}</div>
                         </Container>}
                 position="right center" 
                 offset="0, -4"
