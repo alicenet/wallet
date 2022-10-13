d53f76500553dc50c9f0b59599b01b7c7415ccf8
Ricardo <rfrabasile@gmail.com>
2022-10-13 01:41:17 -0300

    Fix focus trap issue and add action styles

--- app/client/src/components/modals/WalletActionModals/RenameWalletModal.jsx
+++ app/client/src/components/modals/WalletActionModals/RenameWalletModal.jsx
@@ -6,6 +6,7 @@ import { stringUtils } from 'util/_util';
 import { useFormState } from 'hooks/_hooks';
 import { MODAL_ACTIONS, VAULT_ACTIONS } from 'redux/actions/_actions';
 import { electronStoreCommonActions } from 'store/electronStoreHelper';
+import FocusTrap from 'focus-trap-react';
 
 export default function RenameWalletModal() {
 
@@ -53,89 +54,94 @@ export default function RenameWalletModal() {
     return (
 
         <Modal open={isOpen} onClose={closeModal}>
+            <FocusTrap>
+                <div>
+                    <div className="p-4">
 
-            <Modal.Header>
-                <Header as="h4">
-                    Rename Wallet: <span className="text-blue-500">{targetWallet.name}</span>
-                    <Header.Subheader>
-                        Give a friendly name in reference to address: <span className="text-purple-500">{stringUtils.splitStringWithEllipsis(targetWallet.address, 4)}</span>
-                    </Header.Subheader>
-                </Header>
-            </Modal.Header>
+                        <Modal.Header>
+                            <Header as="h4">
+                                Rename Wallet: <span className="text-blue-500">{targetWallet.name}</span>
+                                <Header.Subheader>
+                                    Give a friendly name in reference to address: <span className="text-purple-500">{stringUtils.splitStringWithEllipsis(targetWallet.address, 4)}</span>
+                                </Header.Subheader>
+                            </Header>
+                        </Modal.Header>
 
-            <Modal.Content className="text-sm">
+                        <Modal.Content className="text-sm">
 
-                <p>
-                    Renaming your wallet is considered an administrative action.
-                </p>
-                <p>
-                    Please provide your vault password and desired wallet name below.
-                </p>
+                            <p>
+                                Renaming your wallet is considered an administrative action.
+                            </p>
+                            <p>
+                                Please provide your vault password and desired wallet name below.
+                            </p>
 
-                <Form
-                    error={!!error}
-                    size="small"
-                    className="mt-8"
-                    onSubmit={(e) => {
-                        e.preventDefault();
-                        onSubmit(updateName);
-                    }}
-                >
+                            <Form
+                                error={!!error}
+                                size="small"
+                                className="mt-8"
+                                onSubmit={(e) => {
+                                    e.preventDefault();
+                                    onSubmit(updateName);
+                                }}
+                            >
 
-                    <Form.Group>
+                                <Form.Group>
 
-                        <Form.Input
-                            width={10}
-                            size="small"
-                            label="New Wallet Name"
-                            placeholder="Wallet #2"
-                            value={formState.name.value}
-                            onChange={e => formSetter.setName(e.target.value)}
-                            error={!!formState.name.error && { content: formState.name.error }}
-                        />
-                        <Form.Input
-                            width={6}
-                            type="password"
-                            size="small"
-                            label="Vault Password"
-                            placeholder="Password"
-                            value={formState.password.value}
-                            onChange={e => formSetter.setPassword(e.target.value)}
-                            error={!!formState.password.error && { content: formState.password.error }}
-                        />
+                                    <Form.Input
+                                        width={10}
+                                        size="small"
+                                        label="New Wallet Name"
+                                        placeholder="Wallet #2"
+                                        value={formState.name.value}
+                                        onChange={e => formSetter.setName(e.target.value)}
+                                        error={!!formState.name.error && { content: formState.name.error }}
+                                    />
+                                    <Form.Input
+                                        width={6}
+                                        type="password"
+                                        size="small"
+                                        label="Vault Password"
+                                        placeholder="Password"
+                                        value={formState.password.value}
+                                        onChange={e => formSetter.setPassword(e.target.value)}
+                                        error={!!formState.password.error && { content: formState.password.error }}
+                                    />
 
-                    </Form.Group>
+                                </Form.Group>
 
-                    <Message error size="mini" content="test"/>
+                                <Message error size="mini" content="test"/>
 
-                </Form>
+                            </Form>
 
-            </Modal.Content>
+                        </Modal.Content>
+                    </div>
 
-            <Modal.Actions>
+                    <Modal.Actions className="border-solid border-t-1 border-b-0 border-l-0 border-r-0 border-gray-300 bg-gray-100 p-4">
 
-                <div className="flex justify-between">
+                        <div className="flex justify-between">
 
-                    <Button
-                        size="small"
-                        className="transparent"
-                        content="Close"
-                        onClick={closeModal}
-                        basic
-                    />
+                            <Button
+                                size="small"
+                                className="transparent"
+                                content="Close"
+                                onClick={closeModal}
+                                basic
+                            />
 
-                    <Button
-                        size="small"
-                        content={error ? "Try Again" : "Save Wallet Name"}
-                        color="teal"
-                        onClick={() => onSubmit(updateName)}
-                        loading={loading}
-                    />
+                            <Button
+                                size="small"
+                                content={error ? "Try Again" : "Save Wallet Name"}
+                                color="teal"
+                                onClick={() => onSubmit(updateName)}
+                                loading={loading}
+                            />
 
+                        </div>
+
+                    </Modal.Actions>
                 </div>
-
-            </Modal.Actions>
-
+            </FocusTrap>
         </Modal>
 
     )
